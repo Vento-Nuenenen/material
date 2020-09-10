e69cf292a3b582bd210b89378133e4734373dcec
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2020-09-10 22:04:56 +0200

    Reset anything

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
@@ -14,15 +14,9 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('user_name');
-            $table->string('scout_name');
-            $table->string('first_name');
-            $table->string('last_name');
+            $table->id();
+            $table->string('name');
             $table->string('email')->unique();
-            $table->boolean('active');
-            $table->boolean('initial');
-            $table->boolean('contact');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
