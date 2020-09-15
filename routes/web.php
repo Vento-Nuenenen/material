d6ac372c72704aa82cabcd4c5232b2dbcff77c92
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-09-15 20:31:49 +0200

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +16,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/overwatch', 301);
 
-Auth::routes(['register' => false]);
+Auth::routes();
 
-Route::middleware('auth')->prefix('admin')->group(function(){
-    Route::get('/overwatch', 'HomeController@index')->name('home');
+Route::group(['middleware' => ['auth']], function(){
+    Route::get('/overwatch', 'OverwatchController@index')->name('overwatch');
 
+    Route::any('/users', 'UsersController@index')->name('users');
+    Route::get('/users/add', 'UsersController@create')->name('add-users');
+    Route::post('/users/store', 'UsersController@store')->name('store-users');
+    Route::get('/users/edit/{uid}', 'UsersController@edit')->name('edit-users');
+    Route::post('/users/update/{uid}', 'UsersController@update')->name('update-users');
+    Route::get('/users/destroy/{uid}', 'UsersController@destroy')->name('destroy-users');
+
+    Route::any('/groups', 'GroupsController@index')->name('groups');
+    Route::get('/groups/add', 'GroupsController@create')->name('add-groups');
+    Route::post('/groups/store', 'GroupsController@store')->name('store-groups');
+    Route::get('/groups/edit/{gid}', 'GroupsController@edit')->name('edit-groups');
+    Route::post('/groups/update/{gid}', 'GroupsController@update')->name('update-groups');
+    Route::get('/groups/destroy/{gid}', 'GroupsController@destroy')->name('destroy-groups');
 });
