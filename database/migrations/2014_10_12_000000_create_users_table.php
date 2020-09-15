3e8af75010df6138a426f61a24abef8ef64077e6
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2020-09-15 17:38:12 +0200

    Add migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('scout_name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreignId('group_id');
+            $table->foreignId('role_id');
         });
     }
 
