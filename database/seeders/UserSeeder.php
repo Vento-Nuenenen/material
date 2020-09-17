8841fea1b781a2358bdc46844d0905f598db2332
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2020-09-17 19:32:11 +0200

    Implement user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +15,32 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Seed test admin
+        $seededAdminEmail = 'admin@mat.ch';
+        $user = User::where('email', '=', $seededAdminEmail)->first();
+        if ($user === null) {
+            $user = User::create([
+                'scout_name' => 'Admin',
+                'first_name' => 'Admin',
+                'last_name' => 'Admin',
+                'email' => $seededAdminEmail,
+                'password' => Hash::make('password'),
+            ]);
+            $user->save();
+        }
+
+        // Seed test user
+        $seededUserEmail = 'caspar.brenneisen@protonmail.ch';
+        $user = User::where('email', '=', $seededUserEmail)->first();
+        if ($user === null) {
+            $user = User::create([
+                'scout_name'                     => 'Vento',
+                'first_name'                     => 'Caspar',
+                'last_name'                      => 'Brenneisen',
+                'email'                          => $seededUserEmail,
+                'password'                       => Hash::make('password'),
+            ]);
+            $user->save();
+        }
     }
 }
